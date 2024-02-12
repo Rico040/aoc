df73f5cd8c4a70432c79df0cbf4f032e40f284d8
Rico040 <93081680+Rico040@users.noreply.github.com>
2024-02-12 20:35:23 +1000

    day 1

---
+++ day-1/day-1.py
@@ -0,0 +1,24 @@
+
+
+with open('input.txt', 'r') as file:
+    content = file.read()
+    # print(content)
+
+lines = content.split()
+first = 0
+last = 0
+count = 0
+
+for i in lines:
+    for j in range(len(i)):
+        if i[j].isdigit():
+            first = i[j]
+            break
+    for k in range(len(i)):
+        if i[-k-1].isdigit():
+            last = i[-k-1]
+            break
+    # print(first + last)
+    count += int(first + last)
+
+print(count)
