3a5f3248628344b22a8bdd762f38faefea2cfbc5
Rico040 <93081680+Rico040@users.noreply.github.com>
2024-02-12 22:13:53 +1000

    why do i have zero

--- day-1/main.py
+++ day-1/main.py
@@ -12,12 +12,12 @@ count = 0
 
 def word_to_num(word):
     word_to_num_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
-                        "six": 6, "seven": 7, "eight": 8, "nine": 9, "zero": 0}
+                        "six": 6, "seven": 7, "eight": 8, "nine": 9}
     return word_to_num_dict.get(word, word)
 
 
 for i in lines:
-    matches = re.findall(r'one|two|three|four|five|six|seven|eight|nine|zero|[0-9]', i)
+    matches = re.findall(r'one|two|three|four|five|six|seven|eight|nine|[0-9]', i)
     if not matches[0].isdigit():
         first = word_to_num(matches[0])
     else:
