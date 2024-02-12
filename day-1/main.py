d530562b8b4d82124f9c465746c7069e21d38555
Rico040 <93081680+Rico040@users.noreply.github.com>
2024-02-12 21:07:51 +1000

    nevermind

---
+++ day-1/main.py
@@ -0,0 +1,23 @@
+import re
+
+with open('example.txt', 'r') as file:
+    content = file.read()
+    # print(content)
+
+lines = content.split()
+first = 0
+last = 0
+count = 0
+
+def word_to_num(word):
+    word_to_num_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+                        "six": 6, "seven": 7, "eight": 8, "nine": 9, "zero": 0}
+    return word_to_num_dict.get(word, word)
+
+for i in lines:
+    matches = re.findall(r'one|two|three|four|five|six|seven|eight|nine', i)
+    matches = [word_to_num(word) for word in matches]
+    print(matches)
+    count += int(first + last)
+
+# print(count)
